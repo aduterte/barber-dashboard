f090894fb9d6c25b0b3b4d64348c10fcd84f23bc
aduterte <alexduterte@gmail.com>
2021-03-21 23:40:47 -0400

    added ability to see reviews and reply to them

---
+++ src/components/Reviews.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import API from "../api"
+import { userAtom } from '../atoms';
+import {useRecoilState} from "recoil"
+
+export default function Reviews(props){
+
+    const review = props.review,
+        [reply, setReply] = useState(false),
+        [input, setInput] = useState(""),
+        [user, setUser] = useRecoilState(userAtom)
+
+    const handleInput = (e) => {
+        setInput(e.target.value)
+    }
+
+    const handleReply = () =>{
+        setReply(!reply)
+        setInput("")
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        let data = {content: input, barber_id: user.id, barber_review_id: review.id}
+        // debugger
+        API.post("/barber_review_comments", data)
+        .then( res => {
+            setUser(res.data)
+        })
+    }
+
+    return (
+        <div>
+            {review.client.username}
+            <br/>
+            {review.content}
+            {review.barber_review_comments.length === 0 ? 
+            <div>
+                {reply && 
+                <form onSubmit={handleSubmit}>
+                    <input type="text" value={input} placeholder="Enter Reply" onChange={handleInput}/>
+                    <input type="submit"/>
+                </form>}
+                <div onClick={handleReply}>
+                    {!reply ? "Reply" : "Cancel"}
+                </div>
+            </div>
+            :
+            <div>
+                {review.barber_review_comments[0].content}
+            </div>}
+        </div>
+    )
+}
--- src/containers/DashboardContainer.js
+++ src/containers/DashboardContainer.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import {userAtom} from "../atoms"
+import {useRecoilState} from "recoil"
+import Reviews from '../components/Reviews';
+
 
 export default function DashboardContainer(){
+
+    const [user, setUser] = useRecoilState(userAtom)
+
+
     return(
-        <div>
-            this is the user dashboard
+        <div id="dashboard-container">
+            <div className="dashboard-left">
+                Welcome {user.username}
+                {console.log(user)}
+            </div>
+            <div className="dashboard-right">
+                dashboard details
+            </div>
+            <div className="reviews">
+                {user.barber_reviews.map(review => <Reviews key={`review${review.id}`} review={review}/>)}
+               
+            </div>
+            
         </div>
     )
 }
