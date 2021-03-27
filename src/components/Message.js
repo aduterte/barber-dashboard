63c3cc88d12ec4798d274fdee03d1d7d087926ae
aduterte <60308564+aduterte@users.noreply.github.com>
2021-03-27 17:21:51 -0400

    fixed small bugs

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,13 +3,14 @@ import React from 'react';
 
 
 export default function Message(props){
-    
+
     const {message} = props
    
     function getSender(){
         if (message.sent_by === "barber"){
             return "You"
         } else {
+            // debugger
             return message.client.username
         }
     }
